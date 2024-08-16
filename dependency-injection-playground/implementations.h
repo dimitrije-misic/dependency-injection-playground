b92918bd6bf1d89a7ebe4a623ee70c02fe472f04
Dimitrije Mišić <dimitrije.misic@fazi.rs>
2024-08-16 15:17:31 +0200

    Untabify

--- dependency-injection-playground/implementations.h
+++ dependency-injection-playground/implementations.h
@@ -4,150 +4,150 @@
 
 namespace cisim
 {
-	class Garrett
-	{
-		float _coeff = 0.3f;
+    class Garrett
+    {
+        float _coeff = 0.3f;
 
-	public:
-		uint32_t boost(uint32_t speed)
-		{
-			return uint32_t(speed * _coeff);
-		}
-	};
+    public:
+        uint32_t boost(uint32_t speed)
+        {
+            return uint32_t(speed * _coeff);
+        }
+    };
 
-	template <ITurbo T>
-	class V6Turbo
-	{
-		bool _running = false;
-		int32_t _speed = 0;
+    template <ITurbo T>
+    class V6Turbo
+    {
+        bool _running = false;
+        int32_t _speed = 0;
 
-		T _turbo; // init from provider
+        T _turbo; // init from provider
 
-	public:
-		V6Turbo(T turbo)
-			: _turbo(std::move(turbo))
-		{}
+    public:
+        V6Turbo(T turbo)
+            : _turbo(std::move(turbo))
+        {}
 
-		bool running()
-		{
-			return _running;
-		}
+        bool running()
+        {
+            return _running;
+        }
 
-		uint32_t speed() const
-		{
-			return _speed;
-		}
+        uint32_t speed() const
+        {
+            return _speed;
+        }
 
-		void start()
-		{
-			_running = true;
-		}
+        void start()
+        {
+            _running = true;
+        }
 
-		void accelerate()
-		{
-			_speed += 5 + _turbo.boost(_speed);
-		}
-	};
+        void accelerate()
+        {
+            _speed += 5 + _turbo.boost(_speed);
+        }
+    };
 
-	class V8
-	{
-		bool _running = false;
-		int32_t _speed = 0;
+    class V8
+    {
+        bool _running = false;
+        int32_t _speed = 0;
 
-	public:
-		bool running()
-		{
-			return _running;
-		}
+    public:
+        bool running()
+        {
+            return _running;
+        }
 
-		uint32_t speed() const
-		{
-			return _speed;
-		}
+        uint32_t speed() const
+        {
+            return _speed;
+        }
 
-		void start()
-		{
-			_running = true;
-		}
+        void start()
+        {
+            _running = true;
+        }
 
-		void accelerate()
-		{
-			_speed += 10;
-		}
-	};
+        void accelerate()
+        {
+            _speed += 10;
+        }
+    };
 
-	class Automatic
-	{
-		uint32_t _gear = 0;
-		const uint32_t _max_gear = 8;
+    class Automatic
+    {
+        uint32_t _gear = 0;
+        const uint32_t _max_gear = 8;
 
-	public:
-		void start()
-		{
-			_gear = 1;
-		}
+    public:
+        void start()
+        {
+            _gear = 1;
+        }
 
-		bool needs_shift(uint32_t speed)
-		{
-			return true; // depending on speed and gear determining this
-		}
+        bool needs_shift(uint32_t speed)
+        {
+            return true; // depending on speed and gear determining this
+        }
 
-		void shift(ShiftDirection direction)
-		{
-			_gear = direction == ShiftDirection::Up
-				? std::min(_max_gear, _gear + 1)
-				: std::max(uint32_t(0), _gear - 1);
-		}
-	};
+        void shift(ShiftDirection direction)
+        {
+            _gear = direction == ShiftDirection::Up
+                ? std::min(_max_gear, _gear + 1)
+                : std::max(uint32_t(0), _gear - 1);
+        }
+    };
 
-	class Manual
-	{
-		uint32_t _gear = 0;
-		const uint32_t _max_gear = 5;
+    class Manual
+    {
+        uint32_t _gear = 0;
+        const uint32_t _max_gear = 5;
 
-	public:
-		void start()
-		{
-			_gear = 1;
-		}
+    public:
+        void start()
+        {
+            _gear = 1;
+        }
 
-		bool needs_shift(uint32_t speed)
-		{
-			return true; // depending on speed and gear determining this
-		}
+        bool needs_shift(uint32_t speed)
+        {
+            return true; // depending on speed and gear determining this
+        }
 
-		void shift(ShiftDirection direction)
-		{
-			_gear = direction == ShiftDirection::Up
-				? std::min(_max_gear, _gear + 1)
-				: std::max(uint32_t(0), _gear - 1);
-		}
-	};
+        void shift(ShiftDirection direction)
+        {
+            _gear = direction == ShiftDirection::Up
+                ? std::min(_max_gear, _gear + 1)
+                : std::max(uint32_t(0), _gear - 1);
+        }
+    };
 
-	template <IEngine E, ITransmission T>
-	class FordMustang
-	{
-		E _engine;
-		T _transmission;
+    template <IEngine E, ITransmission T>
+    class FordMustang
+    {
+        E _engine;
+        T _transmission;
 
-	public:
-		FordMustang(E engine, T transmission)
-			: _engine{ std::move(engine) }
-			, _transmission{ std::move(transmission) }
-		{}
+    public:
+        FordMustang(E engine, T transmission)
+            : _engine{ std::move(engine) }
+            , _transmission{ std::move(transmission) }
+        {}
 
-		void start()
-		{
-			_engine.start();
-			_transmission.start();
-		}
+        void start()
+        {
+            _engine.start();
+            _transmission.start();
+        }
 
-		void run()
-		{
-			_engine.accelerate();
+        void run()
+        {
+            _engine.accelerate();
 
-			if (_transmission.needs_shift(_engine.speed()))
-				_transmission.shift(ShiftDirection::Up);
-		}
-	};
+            if (_transmission.needs_shift(_engine.speed()))
+                _transmission.shift(ShiftDirection::Up);
+        }
+    };
 }
--- dependency-injection-playground/interfaces.h
+++ dependency-injection-playground/interfaces.h
@@ -3,39 +3,39 @@
 
 namespace cisim
 {
-	enum class ShiftDirection
-	{
-		Up,
-		Down
-	};
+    enum class ShiftDirection
+    {
+        Up,
+        Down
+    };
 
-	template <typename T>
-	concept ITurbo = requires (T t, uint32_t speed)
-	{
-		{ t.boost(speed) } -> std::convertible_to<uint32_t>;
-	};
+    template <typename T>
+    concept ITurbo = requires (T t, uint32_t speed)
+    {
+        { t.boost(speed) } -> std::convertible_to<uint32_t>;
+    };
 
-	template <typename E>
-	concept IEngine = requires (E e)
-	{
-		{ e.running() } -> std::convertible_to<bool>;
-		{ e.speed() } -> std::convertible_to<uint32_t>;
-		{ e.start() };
-		{ e.accelerate() };
-	};
+    template <typename E>
+    concept IEngine = requires (E e)
+    {
+        { e.running() } -> std::convertible_to<bool>;
+        { e.speed() } -> std::convertible_to<uint32_t>;
+        { e.start() };
+        { e.accelerate() };
+    };
 
-	template <typename T>
-	concept ITransmission = requires (T t, uint32_t speed, ShiftDirection direction)
-	{
-		{ t.start() };
-		{ t.needs_shift(speed) } -> std::convertible_to<bool>;
-		{ t.shift(direction) };
-	};
-	
-	template <typename T>
-	concept IVehicle = requires (T t)
-	{
-		{ t.start() };
-		{ t.run() };
-	};
+    template <typename T>
+    concept ITransmission = requires (T t, uint32_t speed, ShiftDirection direction)
+    {
+        { t.start() };
+        { t.needs_shift(speed) } -> std::convertible_to<bool>;
+        { t.shift(direction) };
+    };
+
+    template <typename T>
+    concept IVehicle = requires (T t)
+    {
+        { t.start() };
+        { t.run() };
+    };
 }
